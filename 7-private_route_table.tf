8206021fc805de9814847ebfc0b60ca4ce3777c6
Krishnadas M <33893081+krishnadasm@users.noreply.github.com>
2019-09-09 17:51:21 +0530

    updating the file to use nat id
    updated

--- 7-private_route_table.tf
+++ 7-private_route_table.tf
@@ -5,9 +5,24 @@ resource "aws_route_table" "private_route" {
 
   route {
     cidr_block     = "0.0.0.0/0"
-    gateway_id     = "${aws_eip.fornat.id}"
+    gateway_id     = "${element(aws_nat_gateway.nat.*.id, 0)}" #here we have set the index manually
   }
   tags   = {
     Name = "Ola-Private-RT"
   }
 }
+
+/*
+  using the following code, we can dynamically adjust the nat id.
+    resource "aws_route_table" "private_route" {
+    vpc_id = "${aws_vpc.my_vpc.id}"
+    route {
+      cidr_block     = "0.0.0.0/0"
+      gateway_id     = "${element(aws_nat_gateway.nat.*.id, index(aws_nat_gateway.nat))}"   using this we can get the count/index of nat gateway.
+    }
+    tags   = {
+      Name = "Ola-Private-RT"
+    }
+  }
+
+*/
